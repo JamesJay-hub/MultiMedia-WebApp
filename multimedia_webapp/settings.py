193a2c5b67ed38a4dac81726b6adfa89bfe9cdcd
James Obatoki <obatokijames@gmail.com>
2025-05-23 11:39:26 +0100

    inital commit

--- multimedia_webapp/settings.py
+++ multimedia_webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z*4n4rs4_^m0_y7q8tvbp+v1$a-k=%@wlfyp-+wxybu_b&10vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
