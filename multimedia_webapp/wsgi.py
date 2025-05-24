d0286a96a60ede9b7e38dba36f167964b28e3796
James Obatoki <obatokijames@gmail.com>
2025-05-24 12:31:58 +0100

    New Changes

--- multimedia_webapp/wsgi.py
+++ multimedia_webapp/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multimedia_webapp.settings')
 
 application = get_wsgi_application()
+
+# #   Allows Django to connect to Vercel
+# app = application
