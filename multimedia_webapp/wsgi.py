cd15394df9a5f2358f0404d1ee14ec6728c1ddaa
James Obatoki <obatokijames@gmail.com>
2025-05-26 06:49:24 +0100

    Fix psycopg2 issue

--- multimedia_webapp/wsgi.py
+++ multimedia_webapp/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multimedia_webapp.settings')
 
 application = get_wsgi_application()
 
-#   Allows Django to connect to Vercel
+#   Vercel Allows Django to connect to Vercel
 app = application
